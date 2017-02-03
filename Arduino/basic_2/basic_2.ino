396a960311a250b88da85d5b7282c227c18d82ce
Benjamin Baer <blueberry@blueberrymuffin.local>
2017-02-03 22:43:45 +0100

    Combined BASIC and LCD Scripts

--- Arduino/basic_2/basic_2.ino
+++ Arduino/basic_2/basic_2.ino
@@ -4,6 +4,66 @@
 SoftwareSerial mySerial(7, 8); // RX, TX
 #define RST  2
 
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 6);
+
+void setup() {
+  
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
+
+
+
+
 // Copy the following lines from TTN Console -> Devices -> Overview tab -> "EXAMPLE CODE"
 const char *devAddr = "26011FB4";
 const char *nwkSKey = "7F17AB5D97EB48CCDB2D0EEDEF964FBA";
@@ -13,6 +73,12 @@ rn2xx3 myLora(mySerial);
 
 // Setup routine runs once when you press reset
 void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Trashmob rocks");
+
+  
   pinMode(13, OUTPUT);
   led_on();
 
@@ -20,6 +86,14 @@ void setup() {
   Serial.begin(57600);
   mySerial.begin(9600);
   Serial.println("Startup");
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 1);
+  // Print a message to the LCD.
+  lcd.print("Trashmob boots up:");
+
+  lcd.begin(16, 2);
+  lcd.print("Starting Lora");
 
   // Reset rn2xx3
   pinMode(RST, OUTPUT);
@@ -38,20 +112,27 @@ void setup() {
 
   myLora.initABP(devAddr, appSKey, nwkSKey);
 
+  lcd.begin(16, 2);
+  lcd.print("Lora started");
+
   led_off();
   delay(2000);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  led_on();
-  Serial.println("TXing");
-
-  myLora.txUncnf("123456789qwertzuiopasdfghjklyxcvbnm+*ç%&/()=QWERTZUIOPASDFGHJKLYXCVBNM");
-
-  led_off();
+  send_data_to_lora();
 
   delay(20000);
+}
+
+void send_data_to_lora() {
+  led_on();
+  lcd.begin(16, 2);
+  lcd.print("Sending to Lora...");
+  Serial.println("TXing");
+  myLora.txUncnf("trashmob.ch was here");
+  led_off();
 }
 
 void led_on()
